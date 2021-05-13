d0271d100bba3199fbedd9fa858719a1cbdc3185
Dani-op <ghaud1@student.op.ac.nz>
2021-05-13 15:38:18 +1200

    added a Visual File to code

---
+++ Coding C sharp with LewisJHill/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Coding_C_sharp_with_LewisJHill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
