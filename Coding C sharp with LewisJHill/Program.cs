7d2af8cb1210a720353fd9ff7402e1ba5c083e2b
LewisJHill <hilllj4@student.op.ac.nz>
2021-05-13 15:56:54 +1200

    Added ending

--- Coding C sharp with LewisJHill/Program.cs
+++ Coding C sharp with LewisJHill/Program.cs
@@ -16,32 +16,18 @@ namespace Coding_C_sharp_with_LewisJHill
             int number5 = 0;
             int number6 = 0;
 
-            for (int i =0; i <= 6000; i++)
-            {
-                dice = rand.Next(7);
-                switch (dice)
-                {
-                    case 1:
-                        number1++;
-                        break;
-                    case 2:
-                        number2++;
-                        break;
-                    case3:
-                        number3++;
-                        break;
-                    case 4:
-                        number4++;
-                        break;
-                    case 5:
-                        number5++;
-                        break;
-                    case 6:
-                        number6++;
-                        break;
-                }
-            }
 
+
+
+
+
+
+
+
+            Console.WriteLine("dice\tnumber");
+            Console.WriteLine("1\t{0}\t{1}\t{2}\t{3}\t{4}\t{5}",
+                number1, number2, number3, number4,
+                number5, number6);
             Console.ReadLine();
         }
     }
